bb3a7aa4774d4c68302c9355a70267b0254240f7
Thomas.Schumacher <Tommyschumacher@gmail.com>
2022-02-24 11:51:41 -0600

    add some google maps api buildout

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
+import { useJsApiLoader } from "@react-google-maps/api"
 
-function App() {
+
+const containerStyle = {
+  width: '800px',
+  height: '750px'
+};
+
+const center = {
+  lat: 41.2565,
+  lng: 95.9345
+};
+
+function MyComponent() {
   return (
-    <div className="App">
-        <h1>Food Truck App</h1>
-    </div>
-  );
+    <LoadScript
+      googleMapsApiKey="keyAIzaSyAXWTvgiIv0FPs4LnmZP8bf0pfv-XHAz14"
+    >
+      <GoogleMap
+        mapContainerStyle={containerStyle}
+        center={center}
+        zoom={10}
+      >
+      <Marker position={{lat: 41.2565,lng: 95.9345}} />
+        { /* Child components, such as markers, info windows, etc. */ }
+        <></>
+      </GoogleMap>
+    </LoadScript>
+  )
 }
 
-export default App;
+export default React.memo(MyComponent)
