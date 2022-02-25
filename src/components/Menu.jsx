9d5e19d4a5dbbb4adcd028b02601f107d8e101e5
Thomas.Schumacher <Tommyschumacher@gmail.com>
2022-02-25 14:56:02 -0600

    Adds navbar css via MUI

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,24 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import { NavLink } from 'react-router-dom';
 
 
-function Menu() {
+export default function NavTabs() {
+  const [value, setValue] = React.useState(0);
 
-    return(
-        <nav className="menu flex">
-            <NavLink to="mapdisplay"> Map </NavLink>
-            <NavLink to="about"> About </NavLink>
-            <NavLink to="login"> Truck Login </NavLink>
-        </nav>
-    )
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <Tabs value={value} onChange={handleChange} aria-label="nav tabs example">
+        <Tab component={NavLink} label="Map" to="mapdisplay" />
+        <Tab component={NavLink} label="About" to="about" />
+        <Tab component={NavLink} label="Truck Login" to="login" />
+      </Tabs>
+    </Box>
+  );
 }
-
-export default Menu;
