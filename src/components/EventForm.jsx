09bfa20102e0e880c54184ac38b05ec236652159
Thomas.Schumacher <Tommyschumacher@gmail.com>
2022-03-09 11:46:03 -0600

    adds locatio & date inputs for create event page

--- src/components/EventForm.jsx
+++ src/components/EventForm.jsx
@@ -3,9 +3,20 @@ import React from "react";
 function EventForm() {
     return(
         <div>
-            Event form works!
+        <h3>Create Event</h3>
+        <div>
+          <label htmlFor="location">Location</label>
+          <input type="text" id="location" />
         </div>
-    )
+        <div>
+          <label htmlFor="date">Date</label>
+          <input  type="date" id="date" />
+        </div>
+        <button >
+          Create 
+        </button>
+      </div>
+    );
 }
 
 export default EventForm;
